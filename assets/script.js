7e9fe32bdd0cdd5114a562385207526e8b3dabab
James Melky <james.melky@hotmail.com>
2020-06-03 11:04:39 +0930

    check user answers true js

--- assets/script.js
+++ assets/script.js
@@ -97,3 +97,32 @@ function generatePassword() {
     }
 
 };
+
+
+  // Checks to make sure user selected ok for all and uses empty minimums from above
+
+  if (numbers === true) {
+    minimumNumbers = functionArray.getNumbers();
+    minimumCount++;
+
+  }
+
+  if (lowerCases === true) {
+    minimumLowerCases = functionArray.getLowerCases();
+    minimumCount++;
+
+  }
+
+  if (upperCases === true) {
+    minimumUpperCases = functionArray.getUpperCases();
+    minimumCount++;
+
+  }
+
+  if (special === true) {
+    minimumSpecialCharacters = functionArray.getSpecialCharacters();
+    minimumCount++;
+
+  }
+
+ 
