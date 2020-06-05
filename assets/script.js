72006f113dc72f1c6db36482ce28b9513e2090ae
James Melky <james.melky@hotmail.com>
2020-06-05 10:53:40 +0930

    finished homework

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,3 @@
-// Special characters for the function created
-const specialCharacters = "!@#$%^&*()";
-const generateButton = document.getElementById('generateBtn')
-generateButton.addEventListener('click', writePassword)
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Prompts that come up after you click generate password
-function generatePassword() {
-  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
-
-  var numbers = confirm("Do you want numbers in your password?");
-
-  var lowerCases = confirm("Do you want lowercases in your password?");
-
-  var upperCases = confirm("Do you want uppercases in your password?");
-
-  var special = confirm("Do you want special characters in your password?");
-
-  // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
 
 
 // Special characters for the function created
@@ -78,6 +37,7 @@ function generatePassword() {
   var minimumUpperCases = "";
   var minimumSpecialCharacters = "";
 
+
   // Generator functions**
   var functionArray = {
     getNumbers: function() {
@@ -97,7 +57,6 @@ function generatePassword() {
     }
 
 };
-
 
   // Checks to make sure user selected ok for all and uses empty minimums from above
 
